24fd313f0e0282cf80e482c02475f860dde89bb5
Harmain1233 <124540022+Harmain1233@users.noreply.github.com>
2024-09-22 01:44:34 -0400

    updated routing and file structure

---
+++ BookStore_Application/frontend/frontend/src/components/Navbar.jsx/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<div className="bg-black text-white px-8 py-4 flex flex-col items-center">
+  <p className="text-xl font-semibold">© {new Date().getFullYear()} Harmain Munir All Rights Reserved.</p>
+  
+  <div className="mt-4 flex gap-4">
+  </div>
+</div>
+
+
+
+  )
+
+}
+
+export default Footer;
