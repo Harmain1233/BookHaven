1467482bcdb9d696bf2db1f452bc37af1a0df30f
Harmain1233 <124540022+Harmain1233@users.noreply.github.com>
2024-10-05 02:01:12 -0400

    created delete/add-to-favorite user feature

--- BookStore_Application/frontend/frontend/src/components/Profile/Favorites.jsx
+++ BookStore_Application/frontend/frontend/src/components/Profile/Favorites.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import FavoriteCard from "../BookCard/FavoriteCard";
 
 const Favorites = () => {
+    const [FavoriteBooks, setFavoriteBooks] = useState([]); // Initialize as an empty array
+    const headers = { 
+        id: localStorage.getItem("id"), 
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+    };
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                
+
+                const response = await axios.get(
+                    "http://localhost:1000/api/v1/get-favorite-books", 
+                    { headers }
+                );
+                setFavoriteBooks(response.data.data);
+            } catch (error) {
+                console.error("Error fetching favorite books:", error);
+            }
+        };
+        fetch();
+    }, [FavoriteBooks]);
+
     return (
-        <div>
-            Favorites
+        <div className="grid grid-cols-4 gap-5 ml-6">
+            {FavoriteBooks && 
+                FavoriteBooks.map((item) => ( // Corrected variable name
+                    <div key={item.id}> 
+                        <FavoriteCard data={item} /> 
+                    </div>
+                ))
+            }
         </div>
     );
 };
