8f4a7f5c64537a21ab4ba94931b855e78ade1f6d
Harmain1233 <124540022+Harmain1233@users.noreply.github.com>
2024-10-08 23:46:52 -0400

    config change

--- BookStore_Application/frontend/frontend/vite.config.js
+++ BookStore_Application/frontend/frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow external access
+    port: 5173, // Specify the port (optional, Vite defaults to 5173)
+  },
 })
